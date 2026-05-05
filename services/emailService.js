21a3f9a53429ee8f36ef58eb558a2d00c3efb06a
Iremide <iremideb82@gmail.com>
2026-05-05 21:50:50 +0100

    Change email transport to use port 587

--- services/emailService.js
+++ services/emailService.js
@@ -7,8 +7,8 @@ dotenv.config();
 // ✅ Create transporter
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // Use SSL
+  port: 587,
+  secure: false, // Use SSL
 auth: {
   user: process.env.EMAIL_USER,
   pass: process.env.EMAIL_PASS,
