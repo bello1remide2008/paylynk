48c34c36bb74c6db7e7a0970177cbc20d36b753f
Iremide <iremideb82@gmail.com>
2026-05-05 16:24:10 +0100

    Update AdminLogin.jsx

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -10,7 +10,7 @@ const AdminLogin = () => {
 
    const handleLogin = async () => {
   try {
-    const res = await fetch("http://localhost:5000/api/admin/auth/login", {
+    const res = await fetch("http://paylynk-1.onrender.com/api/admin/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
