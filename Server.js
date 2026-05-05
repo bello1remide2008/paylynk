6c467c876291d3d52b62d20a873be0bef75d7ee9
Iremide <iremideb82@gmail.com>
2026-05-05 15:04:53 +0100

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import accountRoutes from "./routes/accountRoutes.js";
+import transactionRoutes from "./routes/transactionRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import adminAuthRoutes from "./routes/adminAuthRoutes.js";
+
+import { errorHandler } from "./middleware/errorMiddleware.js";
+
+// 🔥 INIT
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// 🔥 MIDDLEWARE
+app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://10.74.40.24:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  
+  credentials: true
+}));
+// 🔥 ROUTES
+app.use("/api/auth", authRoutes);
+app.use("/api/accounts", accountRoutes);
+app.use("/api/transactions", transactionRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/admin/auth", adminAuthRoutes);
+
+// 🔥 ERROR HANDLER
+app.use(errorHandler);
+
+// 🔥 TEST ROUTE
+app.get("/", (req, res) => {
+  res.send("API running...");
+});
+
+// 🔥 START SERVER
+app.listen(5000, "0.0.0.0", () => {
+  console.log("Server running on port 5000");
+});
