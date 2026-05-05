82d643ae60d81649ab7bdfe8749ab3c6c3aa466d
Iremide <iremideb82@gmail.com>
2026-05-05 22:41:35 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -22,7 +22,7 @@ const app = express();
 // 🔥 MIDDLEWARE
 app.use(express.json());
 app.use(cors({
-  origin: 'https://paylynk-fsfg.vercel.app',
+  origin: 'https://www.paylynkds.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   
   credentials: true
