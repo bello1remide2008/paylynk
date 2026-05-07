eee7e1f4ae616a6d926c86cfa62eff6ff1642675
Iremide <iremideb82@gmail.com>
2026-05-07 07:20:23 +0100

    Add sendMoney route and account ownership check

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,15 +1,42 @@
 import express from "express";
+
 import {
   connectAccount,
   getAccounts,
   setDefaultAccount,
+  sendMoney,
 } from "../controllers/accountController.js";
+
 import { protect } from "../middleware/authMiddleware.js";
+
+import { checkAccountOwnership }
+from "../middleware/accountMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", protect, connectAccount);
-router.get("/", protect, getAccounts);
-router.patch("/default", protect, setDefaultAccount);
+router.post(
+  "/send",
+  protect,
+  checkAccountOwnership,
+  sendMoney
+);
+
+router.post(
+  "/",
+  protect,
+  connectAccount
+);
+
+router.get(
+  "/",
+  protect,
+  getAccounts
+);
+
+router.patch(
+  "/default",
+  protect,
+  setDefaultAccount
+);
 
 export default router;
