d611f2fb1f277b60d2c85c3bf96a045be974c637
Iremide <iremideb82@gmail.com>
2026-05-08 13:04:21 +0100

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -13,25 +13,33 @@ import {
     unfreezeAccount,
 } from "../controllers/adminController.js";
 
-import { protect } from "../middleware/authMiddleware.js";
+
 import { adminProtect } from "../middleware/adminMiddleware.js";
 import { sendAdminOtp } from "../controllers/adminController.js";
 
 const router = express.Router();
 
-router.get("/users", protect, adminProtect, getUsers);
-router.get("/users/:id", protect, adminProtect, getUserDetails);
-router.get("/stats", protect, adminProtect, getAdminStats);
-router.get("/user/:id", protect, adminProtect, getUserProfile);
+router.get("/users", adminProtect, getUsers);
 
-router.patch("/block/:id", protect, adminProtect, blockUser);
-router.patch("/unblock/:id", protect, adminProtect, unblockUser);
+router.get("/users/:id", adminProtect, getUserDetails);
 
-router.post("/reverse/:id", protect, adminProtect, reverseTransaction);
-router.post("/send-otp", protect, adminProtect, sendAdminOtp);
-router.get("/user-by-phone/:phone", protect, adminProtect, getUserByPhone);
-router.post("/send-mail/:id", protect, adminProtect, sendMailToUsers);
-router.post("/freeze-account/:id", protect, adminProtect, freezeAccount);
-router.post("/unfreeze-account/:id", protect, adminProtect, unfreezeAccount);
+router.get("/stats", adminProtect, getAdminStats);
 
+router.get("/user/:id", adminProtect, getUserProfile);
+
+router.patch("/block/:id", adminProtect, blockUser);
+
+router.patch("/unblock/:id", adminProtect, unblockUser);
+
+router.post("/reverse/:id", adminProtect, reverseTransaction);
+
+router.post("/send-otp", adminProtect, sendAdminOtp);
+
+router.get("/user-by-phone/:phone", adminProtect, getUserByPhone);
+
+router.post("/send-mail/:id", adminProtect, sendMailToUsers);
+
+router.post("/freeze-account/:id", adminProtect, freezeAccount);
+
+router.post("/unfreeze-account/:id", adminProtect, unfreezeAccount);
 export default router;
